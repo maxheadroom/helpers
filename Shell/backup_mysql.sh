e4c1649d3ef47c4c0b66a7066b65f12914a5eb17
Falko Zurell <falko@zurell.de>
2015-01-19 17:04:36 +0100

    changed output structure of MySQL backup
    should now allow for easier restore as
    databases stored in individual directories
    
    Change-Id: Ieb9af58103fe10cf1d7d2ea3b56d70af63934534

--- Shell/backup_mysql.sh
+++ Shell/backup_mysql.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# content of the .backup_mysql.conf file:
+
+
+BACKUPDIR="/opt/PHOTOS/backup/database"
+DATESTAMP=`date +"%A-%H"`
+
+
 # MUSER=''
 # MPW=''
 
 source .backup_mysql.conf
 
-if [ -z $MUSER ]: then
+if [ -z $MUSER ]; then
 	echo "You sure you have your username/password in .backup_mysql.conf file?"
 	exit 1
 fi
-
-BACKUPDIR="/opt/backup/mysql"
-DATESTAMP=`date +"%A-%H"`
 
 MYSQLBIN=`which mysql`
 MYSQLDUMPBIN=`which mysqldump`
@@ -43,31 +45,28 @@ fi
 mkdir $BACKUPDIR/${DATESTAMP}
 
 # common options for mysql dump
-DUMPOPTIONS="-u$MUSER --password=$MPW"
+DUMPOPTIONS="-h database.explain-it.org -u$MUSER --password=$MPW"
 DUMPOPTIONS_STRUCTURE="${DUMPOPTIONS} --triggers --routines --comments --add-drop-database --add-drop-table -d"
 DUMPOPTIONS_DATA="${DUMPOPTIONS} --order-by-primary --lock-tables --extended-insert --complete-insert -n -t"
 DUMPOPTIONS_TABLE="${DUMPOPTIONS} --order-by-primary --extended-insert --complete-insert"
 
 function backup_tables {
 	MYDB=$1
-	MYTABLES=`$MYSQLBIN -u${MUSER} --password=${MPW} -e "use ${MYDB}; show tables;" -b --skip-column-names -s`
+	MYTABLES=`$MYSQLBIN $DUMPOPTIONS -e "use ${MYDB}; show tables;" -b --skip-column-names -s`
+    # mkdir $BACKUPDIR/${DATESTAMP}/${MYDB}
 	for table in $MYTABLES; do
 		echo "Dumping table: ${table} of database: ${MYDB}"
-		$MYSQLDUMPBIN $DUMPOPTIONS_TABLE $MYDB $table > $BACKUPDIR/${DATESTAMP}/${MYDB}_${table}.mysql
+		$MYSQLDUMPBIN $DUMPOPTIONS_TABLE $MYDB $table > $BACKUPDIR/${DATESTAMP}/${MYDB}/${table}.mysql
 	done
 }
 
 function backup_schema {
 	MYDB=$1
 	echo "dumping DATABASE structure for $MYDB"
-	$MYSQLDUMPBIN $DUMPOPTIONS_STRUCTURE -B $MYDB > $BACKUPDIR/${DATESTAMP}/${MYDB}_structure.mysql
+    mkdir $BACKUPDIR/${DATESTAMP}/${MYDB}
+	$MYSQLDUMPBIN $DUMPOPTIONS_STRUCTURE -B $MYDB > $BACKUPDIR/${DATESTAMP}/${MYDB}/structure.mysql
 }
 
-function backup_database {
-	MYDB=$1
-	echo "dumping DATABASE: $MYDB"
-	$MYSQLDUMPBIN $DUMPOPTIONS_DATA -B $MYDB > $BACKUPDIR/${DATESTAMP}/${MYDB}_data.mysql
-}
 
 function compress_backup {
 	tar cvfj ${BACKUPDIR}/${DATESTAMP}.backup.tar.bz2 -C ${BACKUPDIR} ${DATESTAMP}/ && rm -rf ${BACKUPDIR}/${DATESTAMP}/
@@ -75,7 +74,7 @@ function compress_backup {
 }
 
 # fetch all databasenames from MySQL
-DATABASES=`$MYSQLBIN -u${MUSER} --password=${MPW} -e "show databases;" -b --skip-column-names -s`
+DATABASES=`$MYSQLBIN $DUMPOPTIONS -e "show databases;" -b --skip-column-names -s`
 
 
 
